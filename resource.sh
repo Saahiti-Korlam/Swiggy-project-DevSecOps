9ce9c98ce1999216f30f926617cdd54a32d83536
SaahitiK <46818707+Saahiti-Korlam@users.noreply.github.com>
2026-04-06 11:43:25 +0530

    Add files via upload

---
+++ resource.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install -y wget curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+# Add Adoptium (Java)
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | \
+sudo tee /etc/apt/keyrings/adoptium.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb \
+$(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | \
+sudo tee /etc/apt/sources.list.d/adoptium.list > /dev/null
+sudo apt update -y
+sudo apt install -y temurin-17-jdk
+java --version
+# Add Jenkins key (fixed method)
+sudo wget -qO /etc/apt/keyrings/jenkins-keyring.asc \
+https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | \
+sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install -y jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+#install docker
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu  
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+
+#install trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
